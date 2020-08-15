4a99f4038a8a0e8492c9e05132c5965db3c25f16
Sergey Kosichkin <d978827@icloud.com>
2020-08-15 14:13:25 +0300

    Initial Commit

---
+++ CityGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityGameApp
+//
+//  Created by Сергей Косичкин on 8/15/20.
+//  Copyright © 2020 Sergey Kosichkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
